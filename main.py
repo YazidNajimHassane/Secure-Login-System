07a6856b5cd3e14eb9bbc58b5027e751ca74fb06
Yazid <yazid@192.168.1.40>
2026-02-19 01:33:46 +0000

    Add login route and user authentication

--- main.py
+++ main.py
@@ -12,10 +12,16 @@ app.config["SQLALCHEMY_TRACK_MODIFICATION"]=False
 db = SQLAlchemy(app)
 
 #databasemodel
-
 class User(db.Model):
-    
+    id=db.Column(db.Integer,primary_key=True)
+    username=db.Column(db.String(25),unique=True,nullable=False)
+    password=db.Column(db.String(150),nullable=False)
 
+    def set_password(self, password):
+        self.password_hash=generate_password_hash(password)
+
+    def check_password(self,password):
+        return  check_password_hash(self.password_hash,password)
 
 @app.route("/")
 def home():
@@ -23,8 +29,20 @@ def home():
         return redirect(url_for('dashboard'))
     return render_template('index.html')
 
+@app.route('/login' , methods=["POST"])
+def login():
+    username=request.form['username']
+    password=request.form['password']
+    username=request.form['username'] 
+    user = User.query.filter_by(username=username).first()
+    if user and user.check_password(password):
+        session['username']= username
+        return redirect(url_for('dashboard'))
+    else :
+        return render_template('index.html')
 
-if __name__ in "__main__":
+
+if __name__ == "__main__":
     with app.app_context():
         db.create_all()
     app.run(debug=True)
